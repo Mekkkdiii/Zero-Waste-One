169412bc31f64ff7624e3b59cb45f11ef540bb02
Hying9503 <hying9503@gmail.com>
2024-11-27 04:35:58 +0800

    Updated

---
+++ .angular/cache/18.2.9/mean-course/vite/deps/chart__js_auto.js
@@ -0,0 +1,13 @@
+import {
+  Chart,
+  registerables
+} from "./chunk-W6SBN6RX.js";
+import "./chunk-WDMUDEB6.js";
+
+// node_modules/chart.js/auto/auto.mjs
+Chart.register(...registerables);
+var auto_default = Chart;
+export {
+  auto_default as default
+};
+//# sourceMappingURL=chart__js_auto.js.map
